29252849330aa78cc0b2d2023eafdfffb5f24c07
reinout <reinout@mobilejazz.com>
2017-08-20 12:25:47 +0200

    remove uglify from build

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,6 @@ const buble = require('rollup-plugin-buble');               // https://buble.sur
 const commonjs = require('rollup-plugin-commonjs');         // https://github.com/rollup/rollup-plugin-commonjs
 const eslint = require('rollup-plugin-eslint');             // https://github.com/TrySound/rollup-plugin-eslint
 const nodeResolve = require('rollup-plugin-node-resolve');  // https://github.com/rollup/rollup-plugin-node-resolve
-const uglify = require('rollup-plugin-uglify');             // https://github.com/TrySound/rollup-plugin-uglify
-
-const isProduction = (process.env.NODE_ENV === 'production');
 
 const plugins = [
     nodeResolve({ jsnext: true, main: true }),
@@ -14,10 +11,6 @@ const plugins = [
 ];
 
 const pkg = require('./package.json');
-
-if (isProduction) {
-    plugins.push(uglify());
-}
 
 module.exports = {
     entry: 'src/index.js',
